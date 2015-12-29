f02dbb65e7de0a394bda55b0ad0d094e38e793f7
Reinder Kamphorst <reinder@toubab.nl>
2015-12-29 13:18:11 +0100

    Namespace comparers

---
+++ Beinder/NamespaceAffinityComparer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Beinder
+{
+    public class NamespaceAffinityComparer : Comparer<string>
+    {
+        readonly string[] _relativeNs;
+
+        public NamespaceAffinityComparer(string relativeNs)
+        {
+            _relativeNs = relativeNs.Split('.');
+        }
+
+        public override int Compare(string x, string y)
+        {
+            return CalculateAffinity(x) - CalculateAffinity(y);
+        }
+
+        int CalculateAffinity(string ns)
+        {
+            var parts = ns.Split('.');
+            int result = 0;
+            while (
+                parts.Length > result &&
+                _relativeNs.Length > result &&
+                Equals(parts[result], _relativeNs[result]))
+                result++;
+            return result;
+        }
+    }
+
+
+}
+
